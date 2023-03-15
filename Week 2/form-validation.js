ef71edb281974f7751386752f6f670798f4857c3
momotimi <michaelmomotimi007@yahoo.com>
2023-03-16 00:54:18 +0100

    week 2 commit

---
+++ Week 2/form-validation.js
@@ -0,0 +1,35 @@
+function validation(){
+    var name = document.getElementById("name");
+    var security = document.getElementById("security");
+    var mail = document.getElementById("email");
+    var phone = document.getElementById("phone");
+    
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+    
+
+    if(name.value.length < 3){
+        alert("The box marded red cannot be empty or less than 3");
+        name.focus();
+        name.style.border = "2px solid red";
+        return false;
+    }else if(phone.value.length < 11){
+        alert("The box marded red cannot be empty or less than 3");
+        phone.focus();
+        phone.style.border = "2px solid red";
+        return false;
+    }else if(security.value.length < 3){
+        alert("The box marded red cannot be empty or less than 3");
+        security.focus();
+        security.style.border = "2px solid red";
+        return false;
+        
+    }else if(mail.value.length < 11){
+        alert("The box marded red cannot be empty or less than 11");
+        mail.focus();
+        mail.style.border = "2px solid red";
+        return false;
+    }
+   
+
+}
